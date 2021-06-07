b31a0b9211c25d2ee304c0494775ca42534dbb7e
Kristina Sananikone <snk.kristina@gmail.com>
2021-06-07 14:20:44 +0200

    Exo cleared, bootstrap added, css and optional to be added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,12 +2,12 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    if params[:answer].downcase == 'i am going to work right now!'
-      params[:answer] = 'Great!'
-    elsif params[:answer].end_with?('?')
-      params[:answer] = 'Silly question, get dressed and go to work!'
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
-      params[:answer] = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
